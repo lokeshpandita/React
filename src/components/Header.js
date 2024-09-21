17c87820e1c03c5a56a3e6261f94084937f94430
Lokesh Pandita <lokeshpandita01@gmail.com>
2024-09-21 12:31:03 +0530

    header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,33 +13,33 @@ const Header = () => {
   console.log(data.loggedInUser);
   console.log(cartItems);
   return (
-    <div className="header flex justify-between  bg-pink-100 mb-2">
+    <div className="header flex justify-between  b-2">
       <div className="logo-container">
-        <img className="logo w-60" src={LOGO_URL}></img>
+        <img className="logo w-60 p-2 rounded-xl" src={LOGO_URL}></img>
       </div>
       <div className="nav-items items-center">
         <ul className="flex p-4 m-4">
           <li className="p-2.5 my-2.5 mx-1.5">
             Status: {isOnline ? "✅" : "🔴"}
           </li>
-          <li className="p-2.5 my-2.5 mx-1.5">
+          <li className="p-2.5 my-2.5 mx-1.5 transition-all duration-300 hover:bg-gray-200 hover:scale-105 rounded-md">
             <Link to="/">Home</Link>
           </li>
-          <li className="p-2.5 my-2.5 mx-1.5">
+          <li className="p-2.5 my-2.5 mx-1.5 transition-all duration-300 hover:bg-gray-200 hover:scale-105 rounded-md">
             <Link to="/about">About Us</Link>
           </li>
-          <li className="p-2.5 my-2.5 mx-1.5">
+          <li className="p-2.5 my-2.5 mx-1.5 transition-all duration-300 hover:bg-gray-200 hover:scale-105 rounded-md">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li className="p-2.5 my-2.5 mx-1.5">
+          <li className="p-2.5 my-2.5 mx-1.5 transition-all duration-300 hover:bg-gray-200 hover:scale-105 rounded-md">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li className="p-2.5 my-2.5 mx-1.5 font-bold text-xl">
+          <li className="p-2.5 my-2.5 mx-1.5 font-bold text-xl transition-all duration-300 hover:bg-blue-200 hover:scale-105 rounded-md">
             <Link to="/cart">Cart {cartItems.length}</Link>
           </li>
-          <li>
+          <li className="transition-all duration-300 hover:bg-gray-200 hover:scale-105 rounded-md">
             <button
-              className="login p-2.5 my-2.5 mx-1.5"
+              className="login p-2.5 my-2.5 mx-1.5 "
               onClick={() => {
                 btnName === "Login"
                   ? setbtnName("Logout")
